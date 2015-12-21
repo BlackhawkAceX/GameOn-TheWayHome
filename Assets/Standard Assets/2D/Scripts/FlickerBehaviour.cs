34c6605eeef62599638b8ba9b5950f0aa55779d8
CarolaC <carolachristoffel@gmx.de>
2015-12-21 23:43:30 +0100

    more sound work...

--- Assets/Standard Assets/2D/Scripts/FlickerBehaviour.cs
+++ Assets/Standard Assets/2D/Scripts/FlickerBehaviour.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class FlickerBehaviour : StateMachineBehaviour {
 
+    FMODUnity.StudioEventEmitter eventEmitter;
+
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        FMODUnity.StudioEventEmitter eventEmitter = animator.gameObject.GetComponent<FMODUnity.StudioEventEmitter>();
+        eventEmitter = animator.gameObject.GetComponent<FMODUnity.StudioEventEmitter>();
 
         if (eventEmitter != null)
             eventEmitter.Play();
@@ -17,9 +19,13 @@ public class FlickerBehaviour : StateMachineBehaviour {
 	//}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (animator.gameObject.transform.parent != null)
+        {
+            if (animator.gameObject.transform.parent.name == "Lanterns")
+                eventEmitter.Stop();
+        }
+	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
