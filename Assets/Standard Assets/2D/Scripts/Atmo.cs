4e403d743381006bed256c863ee943d0e16c3a4b
CarolaC <carolachristoffel@gmx.de>
2016-01-10 20:32:13 +0100

    Background Atmo

---
+++ Assets/Standard Assets/2D/Scripts/Atmo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Atmo : MonoBehaviour {
+
+    private FMOD.Studio.EventInstance atmo_lvl2;
+
+	// Use this for initialization
+	void Start () {
+        atmo_lvl2 = FMODUnity.RuntimeManager.CreateInstance("event:/atmo_lvl2");
+        atmo_lvl2.start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        atmo_lvl2.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
+	}
+}
